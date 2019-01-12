be3c7cbaa60f00356912fa84d88b42c0e3732be5
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2019-01-12 21:53:31 +0530

    Add files via upload

---
+++ week1_q1.cpp
@@ -0,0 +1,34 @@
+/************************************************************************************************
+LINKED LIST
+
+
+#CommentsMatter : Commenting should be used for every executable statement that does not perform trivial functions to non-CS person. 
+
+Implement Singly Linked List using C++. Make sure the following functions: 
+insert(int data) - Adds a new element to the end of the linked list. 
+insertAt(int pos, int data) - It includes the position at which the insertion happens. Positioning starts with 1.  
+	Example: insertAt(1, 3) - inserts 3 at the first position. 
+	It should print a statement “Linked List does not have that many elements” if the position does not exist in the linked list.
+   
+delete() - Deletes the element at the end of the list. 
+deleteAt(int pos) - Deletes the element(or node) at the position pos)
+countItems() - Returns the number of items in the linked list. 
+display() - Displays all the elements in the linked list. 
+	Format should be followed as “1 -> 2 -> 3 -> 4 -> NULL” for a linked list holding integers in the order 1,2,3,4. 
+
+****************************************************************************************************************/
+
+
+
+
+#include<iostream>
+using namespace std;
+
+//create the members of the list
+
+//create the list
+
+int main()
+{
+	return 0;
+}
