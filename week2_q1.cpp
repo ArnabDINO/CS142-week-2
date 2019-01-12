f433d6312e77c0ef30b144b6c2718df54908ede3
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2019-01-12 23:44:11 +0530

    Rename week1_q1.cpp to week2_q1.cpp

--- week2_q1.cpp
+++ week2_q1.cpp
@@ -8,13 +8,13 @@ Implement Singly Linked List using C++. Make sure the following functions:
 insert(int data) - Adds a new element to the end of the linked list. 
 insertAt(int pos, int data) - It includes the position at which the insertion happens. Positioning starts with 1.  
 	Example: insertAt(1, 3) - inserts 3 at the first position. 
-	It should print a statement “Linked List does not have that many elements” if the position does not exist in the linked list.
+	It should print a statement â€œLinked List does not have that many elementsâ€ if the position does not exist in the linked list.
    
 delete() - Deletes the element at the end of the list. 
 deleteAt(int pos) - Deletes the element(or node) at the position pos)
 countItems() - Returns the number of items in the linked list. 
 display() - Displays all the elements in the linked list. 
-	Format should be followed as “1 -> 2 -> 3 -> 4 -> NULL” for a linked list holding integers in the order 1,2,3,4. 
+	Format should be followed as â€œ1 -> 2 -> 3 -> 4 -> NULLâ€ for a linked list holding integers in the order 1,2,3,4. 
 
 ****************************************************************************************************************/
 
